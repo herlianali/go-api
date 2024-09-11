7ac3ada30db2582433fb52e5a218dec1e7032194
herlianali <aliyasa8@gmil.com>
2024-09-11 07:35:56 +0700

    delete script

--- config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import (
-	"log"
-)
-
-func LoadConfig() {
-	log.Println("Configuration loaded successfully.")
-}
--- pkg/utils/response.go
+++
@@ -1,6 +0,0 @@
-package utils
-
-type Users struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
